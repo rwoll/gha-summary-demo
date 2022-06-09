ac3b12f42b0484cc27b04c2c6a83d12ab37aacba
Ross A. Wollman <ross.wollman@gmail.com>
2022-06-09 13:36:39 -0400

    use @actions/core

---
+++ report.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import * as core from '@actions/core';
+
+await core.summary
+.addHeading('Test Results')
+.addCodeBlock('foo', "js")
+.addTable([
+  [{data: 'File', header: true}, {data: 'Result', header: true}],
+  ['foo.js', 'Pass '],
+  ['bar.js', 'Fail '],
+  ['test.js', 'Pass ']
+])
+.addLink('View staging deployment!', 'https://github.com')
+.write()
